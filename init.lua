a1112ae30c5b79d7d332b747d9c1798b23f2305a
Lucas Tavares Sousa <lucastavasousa@gmail.com>
2025-05-29 18:58:19 -0300

    meu primeiro commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = "<Space>"
+vim.o.encoding="utf-8"
+vim.o.fileencoding="utf-8"
+vim.o.bomb = false
+vim.o.shell = "/bin/bash"
+vim.o.fileencodings="utf-8"
+
+
+
+require("config.lazy")
+require("config.keymaps")
+
